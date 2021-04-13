b4bcd1f4d114fdfe54f4b7f1b4837c96e21d1fa9
azzamwildan462 <azzamwildan5@gmail.com>
2021-04-13 20:44:44 +0700

    tes upload

---
+++ magang.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+void tukar(int *angka1, int *angka2)
+{
+    *angka1 = *angka1 ^ *angka2;
+    *angka2 = *angka1 ^ *angka2;
+    *angka1 = *angka1 ^ *angka2;
+}
+int main()
+{
+    int arr[4] = {123, 234, 345, 456};
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("i = %d\nAlamat = %p\nNilai = %d\n", i, (arr + i), *(arr + 1));
+    }
+
+    return 0;
+}
