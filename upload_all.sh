0a1a55ec9cbcd1deec6d3bbfd832649ca2f21512
azzamwildan462 <azzamwildan5@gmail.com>
2021-04-15 16:22:10 +0700

    Update script

--- upload_all.sh
+++ upload_all.sh
@@ -1,8 +1,9 @@
 #Eksperimen 2 
-
+link="https://github.com/azzamwildan462/Magang-IRIS-2021"
 git add .
 git commit -m "$1"
 git push origin main
 echo " "
 echo " "
 echo -n "Upload semua file dengan commit '$1' pada: "; date
+echo "Link: $link"
