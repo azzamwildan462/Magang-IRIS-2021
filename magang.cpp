b620946a2801b52e29aa057ce64e1eeab91af83a
azzamwildan462 <azzamwildan5@gmail.com>
2021-04-15 15:47:34 +0700

    Tes shell script

--- magang.cpp
+++ magang.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iostream>
 void tukar(int *angka1, int *angka2)
 {
     *angka1 = *angka1 ^ *angka2;
@@ -11,12 +12,37 @@ void tukae2(int *angka1, int *angka2)
     *angka1 = *angka2;
     *angka2 = temp;
 }
+
+class student
+{
+private:
+    std::string name;
+    int mark1, mark2, result;
+
+public:
+    student(std::string namet, int mark1t, int mark2t)
+    {
+        name = namet;
+        mark1 = mark1t;
+        mark2 = mark2t;
+    }
+    void calc_media()
+    {
+        result = mark1 + mark2;
+    }
+    void disp()
+    {
+        // printf("Name: %s\n", name);
+        std::cout << name << std::endl;
+        printf("Total: %d\n", result);
+    }
+};
+
 int main()
 {
-    int a = 89, b = 13;
-    printf("Sebelum ditukar:\na = %d, b = %d\n", a, b);
-    tukae2(&a, &b);
-    printf("Setelah ditukar:\na = %d, b = %d\n", a, b);
+    student orang1("Welldone", 3, 4);
+    orang1.calc_media();
+    orang1.disp();
 
     return 0;
 }
---
+++ upload_all.sh
@@ -0,0 +1,5 @@
+#Eksperimen 2 
+
+git add .
+git commit -m "$1"
+git push origin main
