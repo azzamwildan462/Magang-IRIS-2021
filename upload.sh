748148a84c1a1a3f86ad1e947c90a9d4c7321a02
azzamwildan462 <azzamwildan5@gmail.com>
2021-04-15 16:23:22 +0700

    update lagi

--- upload.sh
+++ upload.sh
@@ -1,8 +1,9 @@
 #Eksperimen linux shell
-
+link="https://github.com/azzamwildan462/Magang-IRIS-2021"
 git add "$1"
 git commit -m "$2"
 git push origin main
 echo " "
 echo " "
 echo -n "Upload '$1' dengan commit '$2' pada: "; date
+echo "Link -> $link"
